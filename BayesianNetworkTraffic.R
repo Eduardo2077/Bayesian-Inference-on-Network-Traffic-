5e4ebdf02fb755f8fe1b1700b29b0af186f490e1
Eduardo2077 <63789381+Eduardo2077@users.noreply.github.com>
2020-04-20 22:17:29 -0300

    Update BayesianNetworkTraffic.R

--- BayesianNetworkTraffic.R
+++ BayesianNetworkTraffic.R
@@ -22,10 +22,10 @@
     while(r!=TRUE){
       lambda = runif(1,1,20)
       X2 = rpois(5,lambda)
-      X1 = s.A1%*%(Y-A2%*%X2);X1
+      X1 = s.A1%*%(Y-A2%*%X2)
       r = all(X1>=0)
   }
-    return(list(X1 = X1,X2 = X2))
+    return(list(X1 = X1,X2 = as.matrix(X2,ncol = 1)))
   }
   
 #Solução possível: X1 = [2,0,3,5,5,1,16] e X2 = [6,7,7,10,9]
@@ -35,18 +35,58 @@
   
 # Gerando os lambdas utilizando a posteiori condicional a X
   x0 = X0(Y,A)
-  lambda = list(mapply(rtgamma, n = 1, shape = x0[[1]]+1,scale = 1, b =100),
-  mapply(rtgamma, n = 1, shape = x0[[2]]+1,scale = 1, b =100))
+  lambda = list(X1 = mapply(rtgamma, n = 1, shape = x0[[1]]+1,scale = 1, b =100),
+                X2 = mapply(rtgamma, n = 1, shape = x0[[2]]+1,scale = 1, b =100))
+  lambda$X1 = as.matrix(lambda$X1,ncol = 1)
+  lambda$X2 = as.matrix(lambda$X2,ncol = 1)
 # Definindo a posteriori de Xi dado X2,-i, lambda e Y
   p = function(x,lambdai,X1,lambda1){
     prdt = prod(lambda1^X1/factorial(X1))
-    res = lambda1^x/factorial(x)*prdt
+    res = lambdai^x/factorial(x)*prdt
     return(res)
   }
 # Gerando posteiori condicional para Xi dado X2,-i utilizando Metropolis Hasting within Gibbs
 # 1 - Gerar Xi* ~ P(lambda_i)
 # 2 - Aceitar com probabilidade p.
+  for(j in 1:5000){
+  r = FALSE
+  while(r!=TRUE){
+    v = vector()
+  for(i in 1:5){
+    cond = FALSE
+    while(cond != TRUE){
+      q = rpois(1,lambda$X2[i,j])
+      u = runif(1,0,1)
+      rho = min(1,ppois(x0$X2[i,j],lambda$X2[i,j])/ppois(q,lambda$X2[i,j])*p(q,lambda$X2[i,j],x0$X1[,j],lambda$X1[,j])/p(x0$X2[i,j],lambda$X2[i,j],x0$X1[,j],lambda$X1[,j]))
+      if(u<rho){
+        v = c(v,q)
+        cond = TRUE
+      }
+    }
+  }
+    r = all(s.A1%*%(Y-A2%*%v)>=0)
+  }
   
-      
+  x0$X1 = cbind(x0$X1,s.A1%*%(Y-A2%*%v))
+  x0$X2 = cbind(x0$X2,v)
   
+  lambda$X1 = cbind(lambda$X1,mapply(rtgamma, n = 1, shape = x0$X1[,j+1]+1,scale = 1, b =100))
+  lambda$X2 = cbind(lambda$X2,mapply(rtgamma, n = 1, shape = x0$X2[,j+1]+1,scale = 1, b =100))
+}
+
+  par(mfrow = c(4,3))  
+  for(i in 1:7){
+  barplot(table(x0$X1[i,-c(1:1000)]))
+  }
+  for(i in 1:5){
+    barplot(table(x0$X2[i,-c(1:1000)]))
+  }
+  
+  par(mfrow = c(4,3))  
+  for(i in 1:7){
+    plot(density(lambda$X1[i,-c(1000)]))  
+  }
+  for(i in 1:5){
+    plot(density(lambda$X2[i,-c(1000)]))  
+  }
   
